333158e3f10c09b9363832c0a5d386e899f090cb
zhdt7250 <mohamed.sobhy.ext@orange.com>
2021-10-08 17:25:35 +0200

    gateway implementation running on port 8085

--- gateway/src/main/java/com/gateway/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gateway/gateway/GatewayApplication.java
@@ -2,12 +2,24 @@ package com.gateway.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes().route(r -> r.path("/clinic-service/**")
+						.filters(f -> f.rewritePath("/clinic-service/(?<path>.*)", "/$\\{path}"))
+						.uri("http://localhost:8080"))
+				.build();
 	}
 
 }
